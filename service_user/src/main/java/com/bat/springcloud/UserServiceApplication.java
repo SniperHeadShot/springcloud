65b7899594dbb787038249f8324be980008e1c35
ZhengYu <1584558751@qq.com>
2019-04-01 17:33:59 +0800

    消息总线

--- service_user/src/main/java/com/bat/springcloud/UserServiceApplication.java
+++ service_user/src/main/java/com/bat/springcloud/UserServiceApplication.java
@@ -3,13 +3,11 @@ package com.bat.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableDiscoveryClient
-@RefreshScope
 public class UserServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
--- service_user/src/main/java/com/bat/springcloud/controller/UserAPI.java
+++ service_user/src/main/java/com/bat/springcloud/controller/UserAPI.java
@@ -4,15 +4,27 @@ import com.bat.springcloud.domain.User;
 import com.bat.springcloud.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RefreshScope //使用该注解的类，会在接到SpringCloud配置中心配置刷新的时候，自动将新的配置更新到该类对应的字段中
 public class UserAPI {
 
     @Autowired
     private UserService userService;
+
+/*
+    @Value("${spring1.application1.name1}")
+    private String spring2;
+
+    @GetMapping("/test")
+    public String test() {
+        return spring2;
+    }
+*/
 
     @GetMapping("/user/list")
     public List<User> getUserList(@RequestParam(value = "currentPage") String currentPage, @RequestParam(value = "pageSize") String pageSize, User user) {
