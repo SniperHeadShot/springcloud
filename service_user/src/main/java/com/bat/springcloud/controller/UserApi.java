324c1a0cae55f95ac34797c7ce5903f522b6ce12
ZhengYu <1584558751@qq.com>
2019-05-29 14:50:57 +0800

    生成验证码

--- service_user/src/main/java/com/bat/springcloud/controller/UserApi.java
+++ service_user/src/main/java/com/bat/springcloud/controller/UserApi.java
@@ -1,18 +1,9 @@
 package com.bat.springcloud.controller;
 
 import com.bat.springcloud.service.UserService;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * @ClassName UserApi
- * @Description 用户信息服务
- * @Author ZhengYu
- * @Version: 1.0
- * @Create: 2019/5/24 18:48
- **/
-@Slf4j
 @RestController
 public class UserApi {
 
