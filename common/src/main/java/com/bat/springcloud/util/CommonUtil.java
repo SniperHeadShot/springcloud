c29c6c3dfe10f7844ce098ae5f2a85978e4199b5
ZhengYu <1584558751@qq.com>
2019-06-04 15:51:37 +0800

    规范代码

--- common/src/main/java/com/bat/springcloud/util/CommonUtil.java
+++ common/src/main/java/com/bat/springcloud/util/CommonUtil.java
@@ -58,15 +58,4 @@ public class CommonUtil {
     public static String base64Decrypt(String base64Code) {
         return new String(Base64.decodeBase64(base64Code.getBytes()));
     }
-
-    // 随机出一个boolean值
-    public static boolean getRandomBooleanResult() {
-        return new Random().nextInt(10) > 5;
-    }
-
-    //随机一个用户名
-    public static String getRandomUsername() {
-        String[] usernameArr = {"张三", "李四", "王五", "赵六", "孙权", "刘备", "周瑜", "诸葛", "宋江", "林冲"};
-        return usernameArr[new Random().nextInt(10)];
-    }
 }
