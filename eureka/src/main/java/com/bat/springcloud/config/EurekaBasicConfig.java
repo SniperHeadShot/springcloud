74f925caf21f308993e5b6932aea2ed36f8ef9ee
ZhengYu <1584558751@qq.com>
2019-06-10 21:31:42 +0800

    eureka 注册权限控制

---
+++ eureka/src/main/java/com/bat/springcloud/config/EurekaBasicConfig.java
@@ -0,0 +1,25 @@
+package com.bat.springcloud.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+/**
+ * @ClassName EurekaBasicConfig
+ * @Description 开启  https://blog.csdn.net/jerry_player/article/details/85952023
+ * @Author ZhengYu
+ * @Version: 1.0
+ * @Create: 2019/6/10 21:17
+ **/
+@Configuration
+public class EurekaBasicConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER);
+        http.csrf().disable();
+        // 为了可以使用 http://${user}:${password}@${host}:${port}/eureka/ 这种方式登录,所以必须是httpBasic,如果是form方式,不能使用url格式登录
+        http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+    }
+}
