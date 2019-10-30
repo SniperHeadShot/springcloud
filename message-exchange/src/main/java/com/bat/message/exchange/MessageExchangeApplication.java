17632c9007d99bc28ad3b18fefd87e9df1863642
ZhengYu <1584558751@qq.com>
2019-10-30 16:50:00 +0800

    消息中转服务

---
+++ message-exchange/src/main/java/com/bat/message/exchange/MessageExchangeApplication.java
@@ -0,0 +1,17 @@
+package com.bat.message.exchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 消息处理交换服务
+ *
+ * @author ZhengYu
+ * @version 1.0 2019/10/30 11:01
+ **/
+@SpringBootApplication
+public class MessageExchangeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MessageExchangeApplication.class, args);
+    }
+}
