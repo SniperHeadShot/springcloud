1c4024d77e350f44bafd4d4ca486ecdb8620256f
ZhengYu <1584558751@qq.com>
2019-10-30 16:48:33 +0800

    注册中心 - 服务端

--- eureka/eureka-server/src/main/java/com/bat/eureka/server/EurekaServerApplication.java
+++ eureka/eureka-server/src/main/java/com/bat/eureka/server/EurekaServerApplication.java
@@ -1,19 +1,19 @@
-package com.bat.springcloud.eureka;
+package com.bat.eureka.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
- * Eureka 服务注册中心
+ * 注册中心
  *
  * @author ZhengYu
- * @version 1.0 2019/7/11 16:46
+ * @version 1.0 2019/10/30 10:30
  **/
-@SpringBootApplication
 @EnableEurekaServer
-public class EurekaApplication {
+@SpringBootApplication
+public class EurekaServerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(EurekaApplication.class, args);
+        SpringApplication.run(EurekaServerApplication.class, args);
     }
 }
