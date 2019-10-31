1a87ad7478d847c699bce5cf6c5caa66cca5945d
ZhengYu <1584558751@qq.com>
2019-10-31 20:23:02 +0800

    注册中心 - 客户端

---
+++ eureka/eureka-client/src/main/java/com/bat/eureka/client/controller/CustomController.java
@@ -0,0 +1,93 @@
+package com.bat.eureka.client.controller;
+
+import com.alibaba.fastjson.JSONObject;
+import com.bat.commoncode.entity.CustomStructure;
+import com.bat.commoncode.enums.ConstantEnum;
+import com.bat.commoncode.exceptions.SurpriseException;
+import com.bat.commoncode.response.CommonResult;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
+import java.util.Random;
+
+/**
+ * 测试 Controller
+ *
+ * @author ZhengYu
+ * @version 1.0 2019/10/30 14:57
+ **/
+@Slf4j
+@RestController
+@RequestMapping("/custom")
+public class CustomController {
+
+    @GetMapping("/get")
+    public CommonResult customGet(HttpServletRequest request) {
+        parseRequestParam(request, null);
+        buildSurprise();
+        return CommonResult.buildCommonResult(ConstantEnum.GLOBAL_SUCCESS);
+    }
+
+    @PostMapping("/post")
+    public CommonResult customPost(@RequestBody CustomStructure customStructure, HttpServletRequest request) {
+        parseRequestParam(request, customStructure);
+        buildSurprise();
+        return CommonResult.buildCommonResult(ConstantEnum.GLOBAL_SUCCESS);
+    }
+
+    @PutMapping("/put")
+    public CommonResult customPut(@RequestBody CustomStructure customStructure, HttpServletRequest request) {
+        parseRequestParam(request, customStructure);
+        buildSurprise();
+        return CommonResult.buildCommonResult(ConstantEnum.GLOBAL_SUCCESS);
+    }
+
+    @DeleteMapping("/delete")
+    public CommonResult customDelete(@RequestBody CustomStructure customStructure, HttpServletRequest request) {
+        parseRequestParam(request, customStructure);
+        buildSurprise();
+        return CommonResult.buildCommonResult(ConstantEnum.GLOBAL_SUCCESS);
+    }
+
+    /**
+     * 随机产生一个异常
+     *
+     * @author ZhengYu
+     */
+    private void buildSurprise() {
+        if (new Random().nextBoolean()) {
+            throw new SurpriseException();
+        }
+    }
+
+    /**
+     * 解析Http请求参数
+     *
+     * @param request request
+     * @param o       请求体
+     * @author ZhengYu
+     */
+    private void parseRequestParam(HttpServletRequest request, Object o) {
+        // 读取请求参数
+        Enumeration<String> parameterNameEnumeration = request.getParameterNames();
+        while (parameterNameEnumeration.hasMoreElements()) {
+            String paramKey = parameterNameEnumeration.nextElement();
+            String paramValue = request.getParameter(paramKey);
+            log.info("Http请求，请求参数 Key=[{}], Value=[{}]", paramKey, paramValue);
+        }
+        // 读取请求头
+        Enumeration<String> headersEnumeration = request.getHeaderNames();
+        while (headersEnumeration.hasMoreElements()) {
+            String headerKey = headersEnumeration.nextElement();
+            String headerValue = request.getHeader(headerKey);
+            log.info("Http请求，请求头   Key=[{}], Value=[{}]", headerKey, headerValue);
+        }
+        // 读取请求体
+        if (o == null) {
+            return;
+        }
+        log.info("Http请求，请求体   [{}]", JSONObject.toJSONString(o));
+    }
+}
